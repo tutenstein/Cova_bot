c4073cf0ff578a5599d236b52bb11ef8a1e64404
tutenstein <58851436+tutenstein@users.noreply.github.com>
2022-09-01 19:34:30 +0300

    Update QAbot.py

--- QAbot.py
+++ QAbot.py
@@ -1,10 +1,10 @@
-
 import json
 import pandas as pd
 import numpy as np
 from Covabot import pre_process,cova_reply_bot
 import gensim
-import random
+
+
 
 stackoverflow_path = 'data/Word2Vec_data.json'
 
@@ -38,23 +38,24 @@ def greeting(sentence):
 
 
 
+
 flag_language = True
 flag_query = True
 dict_category = {'0': 'internship', '1': 'erasmus', '2': 'summer', '3': 'contact', '4': 'thesis', '5': 'other'}
 
 print('......................................................................................')
-print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + 'My name is CUVA, a Çukurova University Computer Eng. Q&A Bot.')
-print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + 'I will try my best to answer your query.')
-print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + 'If you want to exit, you can type < bye >.')
+print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + 'My name is COVA, a Çukurova University Computer Eng. Q&A Bot.')
+print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + 'I will try my best to answer your query.')
+print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + 'If you want to exit, you can type < bye >.')
 
 while(flag_language == True):
     print("......................................................................................")
-    print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + 'Please select which category you want to enquire, ' +
+    print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + 'Please select which category you want to enquire, ' +
       'you can type:')
-    print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + '< 0 > for internship     < 1 > for erasmus      < 2 > for summer')
-    print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + '< 3 > for contact       < 4 > for thesis      < 5 > for other')
+    print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + '< 0 > for internship    < 1 > for erasmus     < 2 > for summer')
+    print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + '< 3 > for contact       < 4 > for thesis      < 5 > for other')
     print("......................................................................................")
-    sentence = input('\x1b[0;30;47m' + 'USER  ' + '\x1b[0m' + ':')
+    sentence = input('\x1b[0;30;44m' + 'USER  ' + '\x1b[0m' + ':')
     print("......................................................................................")
     
     if(sentence.lower() != 'bye'):
@@ -78,27 +79,26 @@ while(flag_language == True):
         flag_query = False
 
 print("......................................................................................")
-print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + 'Let''s start! Please input your question now.')
+print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + 'Let''s start! Please input your question now.')
     
 while(flag_query == True):
     print("......................................................................................")
-    sentence = input('\x1b[0;30;47m' + 'USER  ' + '\x1b[0m' + ':')
+    sentence = input('\x1b[0;30;44m' + 'USER  ' + '\x1b[0m' + ':')
     print("......................................................................................")
 
     if(sentence.lower() != 'bye'):
         if(greeting(sentence.lower()) != None):
-            print('\x1b[1;37;40m' + 'COVA' + '\x1b[0m' + ': ' + greeting(sentence.lower()))
+            print('\x1b[1;40;42m' + 'COVA' + '\x1b[0m' + ': ' + greeting(sentence.lower()))
         else:
             reply, score,replies = cova_reply_bot(data_language, model,sentence)
             if(len(replies)!=0):
-                print('\x1b[1;37;40m' + 'COVA'+'\x1b[0m'+': '+reply)
-                print('\x1b[1;37;40m' + 'COVA'+'\x1b[0m'+': '+'Did you want to ask this questions?\n' +data_language['question'][replies['index'][1]]+'\n '+data_language['question'][replies['index'][2]])
+                print('\x1b[1;40;42m' + 'COVA'+'\x1b[0m'+': '+reply)
+                print('\x1b[1;40;42m' + 'COVA'+'\x1b[0m'+': '+'Did you want to ask this questions?\n' +data_language['question'][replies['index'][1]]+'\n '+data_language['question'][replies['index'][2]])
             else:
-                print('\x1b[1;37;40m' + 'COVA'+'\x1b[0m'+': '+reply)
+                print('\x1b[1;40;42m' + 'COVA'+'\x1b[0m'+': '+reply)
             #For Tracing, comment to remove from print 
             #print("")
             #print("SCORE: " + str(score))
     else:
         flag_query = False
-print('\x1b[1;37;40m' + 'JARVIS' + '\x1b[0m' + ': ' + 'Bye! Hope that i am of help.') 
-
+print('\x1b[1;30;42m' + 'COVA' + '\x1b[0m' + ': ' + 'Bye! Hope that i am of help.') 
